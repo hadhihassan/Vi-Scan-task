ef2530e89cdbfae0a87bd4ff04f00048e6ae1149
hadhi hassan m <diluhadhi01@gmail.com>
2025-01-01 11:12:02 +0530

    host: Server url changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL : 'http://localhost:3000/api',
+    baseURL : 'https://vi-scan-task.onrender.com/api',
     withCredentials:true    
 })
